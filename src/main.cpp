db0194cc1556fc817ba39a9e2f4f5c87a99a0bb2
Wi-Fighter2006 <piyushchudhary97@gmail.com>
2026-02-20 11:14:21 +0530

    Add Touch Detection Logic + Delay

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
  * @brief Embedded Touch Detection System using TTP223
  * @author Piyush Choudhary
  * @date 2026-02-20
+ *
+ * @details
+ * Detects capacitive touch using TTP223 module
+ * and prints status messages to the Serial Monitor.
  */
 
 #define TOUCH_PIN 2
@@ -21,11 +25,17 @@ void setup() {
 
 void loop() {
 
-    // STEP 4: Read digital value from touch sensor
+    // Read digital value
     touchState = digitalRead(TOUCH_PIN);
 
-    Serial.print("Touch State: ");
-    Serial.println(touchState);
+    // If touch detected (TTP223 outputs HIGH on touch)
+    if (touchState == HIGH) {
+        Serial.println("Touch Detected");
+    } 
+    else {
+        Serial.println("No Touch");
+    }
 
-    delay(200);
+    // Small delay for readability
+    delay(300);
 }
