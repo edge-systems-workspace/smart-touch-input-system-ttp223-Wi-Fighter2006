74e2c6d4f9ae9d298d378a00a5fd6c50a823f035
Wi-Fighter2006 <piyushchudhary97@gmail.com>
2026-02-20 11:12:19 +0530

    Initialize Serial Communication

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,16 @@
  * @brief Embedded Touch Detection System using TTP223
  * @author Piyush Choudhary
  * @date 2026-02-20
- *
- * @details
- * Reads digital touch input from a TTP223 capacitive touch sensor
- * connected to TOUCH_PIN and prints touch status to Serial Monitor.
  */
 
-// STEP 1: Define touch sensor digital pin
 #define TOUCH_PIN 2
 
-// Variable to store touch state
 int touchState = 0;
 
 void setup() {
+
+    // STEP 2: Initialize Serial communication
+    Serial.begin(9600);
 }
 
 void loop() {
